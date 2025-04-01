3e516295c96dc87ed8d90155b68775216ba8ba1c
Royston Joel <65667504+RoystonJoel@users.noreply.github.com>
2025-04-01 16:24:42 +0530

    Foundation for Appointment test

--- CURA Healthcare Service/Test_Appointment.cs
+++ CURA Healthcare Service/Test_Appointment.cs
@@ -1,6 +1,35 @@
-﻿namespace Roys_Selenium_Portfolio;
+﻿using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using FluentAssertions;
+using System.Diagnostics;
+using Xunit.Abstractions;
 
-public class Test_Appointment
+namespace Roys_Selenium_Portfolio
 {
     
+    public class Test_Appointment
+    {
+        private readonly ITestOutputHelper output;
+        private readonly ChromeOptions _options;
+        private readonly ChromeDriver driver;
+        private readonly WebDriverWait wait;
+        private readonly string[] scopes;
+        
+        public Test_Appointment(ITestOutputHelper output)
+        {
+            _options = new ChromeOptions();
+            //_options.AddArgument("--headless");
+            this.output = output;
+            driver = new ChromeDriver(_options);
+            driver.Manage().Window.Maximize(); //fullscreen
+        }
+
+        [Fact]
+        public void correct_login()
+        {
+            
+        }
+
+
+    }
 }
