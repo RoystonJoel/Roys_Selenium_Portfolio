1f853b123a7fccd5c07be8567a81769b50f3ba7d
Royston Joel <65667504+RoystonJoel@users.noreply.github.com>
2025-04-02 19:05:17 +0530

    Assertions added for successful appointment & Appointment Confirmation methods added
    Signed-off-by: Royston Joel <65667504+RoystonJoel@users.noreply.github.com>

---
+++ CURA Healthcare Service/Page Object/AppointmentConfirmation.cs
@@ -0,0 +1,46 @@
+﻿namespace Roys_Selenium_Portfolio;
+using OpenQA.Selenium.Chrome;
+
+public class AppointmentConfirmation
+{
+    private readonly SeleniumHelper _helper;
+
+    public AppointmentConfirmation(ChromeDriver driver)
+    {
+        _helper = new SeleniumHelper(driver);
+    }
+
+    public string facility()
+    {
+        return _helper.grabtext().ById("facility");
+    }
+    
+    public string hospital_readmission()
+    {
+        return _helper.grabtext().ById("hospital_readmission");
+    }
+    
+    public string program()
+    {
+        return _helper.grabtext().ById("program");
+    }
+    
+    public string visit_date()
+    {
+        return _helper.grabtext().ById("visit_date");
+    }
+    
+    public string comment()
+    {
+        return _helper.grabtext().ById("comment");
+    }
+    
+    public SeleniumHelper GetHelper()
+    {
+        return _helper;
+    }
+    public void Dispose()
+    {
+        _helper.Dispose();
+    }
+}
--- CURA Healthcare Service/Test_Appointment.cs
+++ CURA Healthcare Service/Test_Appointment.cs
@@ -28,17 +28,48 @@ namespace Roys_Selenium_Portfolio
         [Fact]
         public void Appointment_Success ()
         {
+            string facility = "Hongkong CURA Healthcare Center";
+            string healthcare_Program = "radio_program_medicaid";
+            string visit_date = "23/04/2025";
+            
             var login = new Login(driver);
             login.auto_login();
             var appointment = new Appointment(login.GetHelper().GetDriver());
             appointment.GetHelper().wait().wait_until_visable_byID("combo_facility");
-            appointment.choose_facility("Hongkong CURA Healthcare Center");
+            appointment.choose_facility(facility);
             appointment.hospital_readmission();
-            appointment.healthcare_Program();
-            appointment.visit_date();
+            appointment.healthcare_Program(healthcare_Program);
+            appointment.visit_date(visit_date);
             appointment.comment("Test comment");
+            string previousHtml = login.GetHelper().GetDriver().PageSource;
             appointment.book_appointment();
+            var appointment_confirmation = new AppointmentConfirmation(appointment.GetHelper().GetDriver());
+            appointment.GetHelper().wait().wait_until_visable_byID("summary");
+            string currentHtml = login.GetHelper().GetDriver().PageSource;
+            
+            try
+            {
+                var url = appointment_confirmation.GetHelper().GetDriver().Url;
+                url.Should().NotBeEmpty();
+                //url.Should().Contain("appointment.php#summary");
+                url.Should().NotBeNull();
+                currentHtml.Should().NotBe(previousHtml);
+                
+                appointment_confirmation.facility().Should().Contain(facility);
+                appointment_confirmation.program().ToLower().Should().Contain(healthcare_Program.Replace("radio_program_", ""));
+                appointment_confirmation.visit_date().Should().Contain(visit_date);
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.StackTrace);
+                appointment_confirmation.Dispose();
+                throw;
+            }
+            appointment_confirmation.Dispose();
         }
+        
+        
         
 
 
