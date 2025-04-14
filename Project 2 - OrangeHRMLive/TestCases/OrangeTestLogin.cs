bfa5fab946fad0bdf837ac85fe67f612fe9fb1ed
Royston Joel <65667504+RoystonJoel@users.noreply.github.com>
2025-04-14 23:13:23 +0530

    Login Test case added, without assertions
    Signed-off-by: Royston Joel <65667504+RoystonJoel@users.noreply.github.com>

---
+++ Project 2 - OrangeHRMLive/TestCases/OrangeTestLogin.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestPlatform.ObjectModel;
+using Xunit.Abstractions;
+
+namespace Roys_Selenium_Portfolio.Project_2___OrangeHRMLive;
+
+public class OrangeTestLogin : TestBase
+{
+    public OrangeTestLogin(ITestOutputHelper output) : base(output)
+    {
+    }
+
+    [Fact]
+    public void correct_login()
+    {
+        var login = new OrangeLogin(_driver);
+        login.EnterUsername();
+        login.EnterPassword();
+        login.Submit();
+        Thread.Sleep(9000);
+    }
+}
