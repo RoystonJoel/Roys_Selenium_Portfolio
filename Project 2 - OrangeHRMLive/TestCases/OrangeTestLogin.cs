4dd7c8bf297dbe2358122a70e65f8c4d8307e0eb
Royston Joel <65667504+RoystonJoel@users.noreply.github.com>
2025-04-16 20:25:59 +0530

    New testcase for a custom login
    Signed-off-by: Royston Joel <65667504+RoystonJoel@users.noreply.github.com>

--- Project 2 - OrangeHRMLive/TestCases/OrangeTestLogin.cs
+++ Project 2 - OrangeHRMLive/TestCases/OrangeTestLogin.cs
@@ -10,11 +10,22 @@ public class OrangeTestLogin : TestBase
     }
 
     [Fact]
-    public void correct_login()
+    public void correctLogin()
     {
         var login = new OrangeLogin(_driver);
         login.EnterUsername();
         login.EnterPassword();
+        login.Submit();
+        Thread.Sleep(9000);
+    }
+
+    [Fact]
+    public void CustomLogin()
+    {
+        initlizeSecretsOrangeHrm();
+        var login = new OrangeLogin(_driver);
+        login.EnterUsername(credentials.Username);
+        login.EnterPassword(credentials.Password);
         login.Submit();
         Thread.Sleep(9000);
     }
