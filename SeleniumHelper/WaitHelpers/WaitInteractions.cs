b3f48f0c91c51a5879b20e07cbce34e1c3c4f7f1
Royston Joel <65667504+RoystonJoel@users.noreply.github.com>
2025-05-18 15:33:24 +0530

    Added WaitInteractions.cs to reduce repeated code
    Signed-off-by: Royston Joel <65667504+RoystonJoel@users.noreply.github.com>

---
+++ SeleniumHelper/WaitHelpers/WaitInteractions.cs
@@ -0,0 +1,37 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+using SeleniumExtras.WaitHelpers;
+
+namespace Roys_Selenium_Portfolio;
+
+public class WaitInteractions
+{
+    private readonly WebDriverWait _wait;
+    
+    public WaitInteractions(WebDriverWait wait)
+    {
+        _wait = wait;
+    }
+
+    //waitUntil takes a Func<IWebDriver, TResult?> as a parameter, which is exactly what _wait.Until() expects.
+    //TResult : notnull constraint ensures that the result is not null.
+    public TResult WaitUntil<TResult>(Func<IWebDriver, TResult?> condition) where TResult : notnull
+    { 
+        return _wait.Until(condition);
+    }
+
+    public void UntilVisible(By by)
+    {
+        WaitUntil(ExpectedConditions.ElementIsVisible(by));
+    }
+
+    public bool TextToBePresentInElementLocated(By by, string text)
+    {
+        return WaitUntil(ExpectedConditions.TextToBePresentInElementLocated(by, text));
+    }
+
+    public void UntilClickable(By by)
+    {
+        WaitUntil(ExpectedConditions.ElementToBeClickable(by));
+    }
+}
